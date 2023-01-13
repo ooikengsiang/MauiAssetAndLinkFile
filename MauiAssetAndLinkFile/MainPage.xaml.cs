5b3b3b9ee34822eb1bfcf16835f249d4996141ba
Ooi Keng Siang <ooiks@live.com>
2023-01-13 21:02:23 +0800

    First commit
    First commit

---
+++ MauiAssetAndLinkFile/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiAssetAndLinkFile;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		_ = Load();
+    }
+
+	public async Task Load()
+	{
+        var isAboutAssetsFound = await FileSystem.Current.AppPackageFileExistsAsync("AboutAssets.txt");
+        AboutAssetsResultLabel.Text = isAboutAssetsFound.ToString();
+
+        var isDontReadMeFound = await FileSystem.Current.AppPackageFileExistsAsync("DontReadMe.txt");
+        DontReadMeResultLabel.Text = isDontReadMeFound.ToString();
+    }
+}
---
+++ MauiAssetAndLinkFile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAssetAndLinkFile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
